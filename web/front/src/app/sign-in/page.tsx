24c9310817e01f567fc8180bd2d4ec2f6d620336
Coding-With-Josh <codewithjoshh@gmail.com>
2025-01-01 17:58:04 +0100

    ss

--- web/front/src/app/sign-in/page.tsx
+++ web/front/src/app/sign-in/page.tsx
@@ -1,14 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { SignIn } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
-import { redirect } from "next/navigation";
 
-import React from "react";
-
-const Page = async () => {
-
-
+const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen gap-[2rem]">
       <h2 className="font-bold text-xl">
